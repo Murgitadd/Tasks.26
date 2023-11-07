9ba892035db3d5d561bbf5364725ac2c70f65bef
Murad <145739695+Murgitadd@users.noreply.github.com>
2023-11-07 08:10:38 +0400

    Added Variation script.js

--- script.js
+++ script.js
@@ -14,14 +14,36 @@ var tableBody = table.querySelector("tbody");
 
 
 
+//==========================================================
+//====================With insert===========================
+//==========================================================
+// people.forEach(function(person) {
+//     var row = tableBody.insertRow(tableBody.rows.length);
+//     var content1 = row.insertCell(0);
+//     var content2 = row.insertCell(1);
+//     var content3 = row.insertCell(2);
 
+//     content1.textContent = person.name;
+//     content2.textContent = person.surname;
+//     content3.textContent = person.age;
+// });
+//==========================================================
+//================With appendChild==========================
+//==========================================================
 people.forEach(function(person) {
-    var row = tableBody.insertRow(tableBody.rows.length);
-    var content1 = row.insertCell(0);
-    var content2 = row.insertCell(1);
-    var content3 = row.insertCell(2);
+    var row = document.createElement("tr");
+
+    var content1 = document.createElement("td");
+    var content2 = document.createElement("td");
+    var content3 = document.createElement("td");
 
     content1.textContent = person.name;
     content2.textContent = person.surname;
     content3.textContent = person.age;
+
+    row.appendChild(content1);
+    row.appendChild(content2);
+    row.appendChild(content3);
+
+    tableBody.appendChild(row);
 });
